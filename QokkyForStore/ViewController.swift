9cdc7dd6ae07b177d30ef4e4fd80992d0080cace
Phongsakorn <Srikate123@hotmailc.com>
2020-06-16 02:37:59 +0700

    Initial Commit

---
+++ QokkyForStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QokkyForStore
+//
+//  Created by Phongsakorn Srikate on 16/6/2563 BE.
+//  Copyright © 2563 Phongsakorn Srikate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
